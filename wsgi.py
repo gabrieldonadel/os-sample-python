1bd2441786ae73e3b1eeaeac2403aca5fd0dec4c
Gabriel Donadel <gabidonadel1@gmail.com>
2019-08-17 09:30:40 -0300

    ibm

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,20 @@ application = Flask(__name__)
 def hello():
     return "OpenShift Hello World!"
 
+    
+@application.route('/')
+def main():
+    return "Hello word"
+
+@application.route('/iot', methods=['GET', 'POST'])
+def index():
+    return "IOT"
+
+@application.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
 if __name__ == "__main__":
     application.run()
